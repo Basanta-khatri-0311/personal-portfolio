b2af925fb3ee6b36634250ea33e5218d18eb9a9b
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-22 13:45:11 +0545

    fonts updating

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Hero = ()=>{
-    return(
-        <section className='w-screen h-screen bg-green-400 flex items-center justify-center'>
-            <div className=''>
-                <h1 className='text-7xl'>Hi, I'm Basanta Khatri</h1>
-                <p className=''>A passionate Frontend Developer and Data Enthusiast</p>
-                <button className=''>View my Work</button>
-            </div>
-        </section>
-    )
-}
+const Hero = () => {
+  return (
+    <section className="w-screen h-screen bg-purple-800 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-7xl font-semibold text-pink-200 font-sans">
+          Hi, I'm Basanta Khatri
+        </h1>
+        <p className="text-lg text-white mt-4">
+          A passionate Frontend Developer and Data Enthusiast
+        </p>
+        <button className="mt-6 px-6 py-3 bg-orange-400  text-white  rounded-lg shadow-lg hover:bg-orange-500 hover:text-white transition duration-300">
+          View my Work
+        </button>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
