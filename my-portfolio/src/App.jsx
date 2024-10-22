482f0d5ab6f756e91a78405b2957a4a36dbed662
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-10-22 13:23:04 +0545

    created components

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import React from "react"
+import Hero from "./components/Hero"
+// import About from "./components/About"
+// import Projects from "./components/Projects"
+// import Contact from "./components/Contact"
+// import Footer from "./components/Footer"
 
 function App() {
 
   return (
     <div>
-      
+      <Hero/>
     </div>
   )
 }
---
+++ my-portfolio/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = ()=>{
+    return(
+        <section className='w-screen h-screen bg-green-400 flex items-center justify-center'>
+            <div className=''>
+                <h1 className='text-7xl'>Hi, I'm Basanta Khatri</h1>
+                <p className=''>A passionate Frontend Developer and Data Enthusiast</p>
+                <button className=''>View my Work</button>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
