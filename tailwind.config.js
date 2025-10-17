ff4dd12826cc9a23345a737ef7047d606622b15a
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-10-17 15:07:00 +0545

    added styles included

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,49 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
+      animation: {
+        'blob': 'blob 7s infinite',
+        'fadeInUp': 'fadeInUp 0.6s ease-out',
+        'slideInLeft': 'slideInLeft 0.8s ease-out',
+      },
+      keyframes: {
+        blob: {
+          '0%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+          '100%': { transform: 'translate(0px, 0px) scale(1)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(30px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        slideInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-30px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+      },
+      backdropBlur: {
+        xs: '2px',
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [
+    function({ addUtilities }) {
+      addUtilities({
+        '.line-clamp-3': {
+          display: '-webkit-box',
+          '-webkit-line-clamp': '3',
+          '-webkit-box-orient': 'vertical',
+          'overflow': 'hidden',
+        },
+        '.animation-delay-2000': {
+          'animation-delay': '2s',
+        },
+        '.animation-delay-4000': {
+          'animation-delay': '4s',
+        },
+      })
+    }
+  ],
 }
