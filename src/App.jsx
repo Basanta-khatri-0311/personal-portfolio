77b1c33ffb9cbc7c54a69f1bbaaa391472e0d13b
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-10-17 15:04:58 +0545

    removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import React, { Suspense, lazy } from "react";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import Loading from "./components/Loading"; // Import the new Loading component
-
-// Lazy-loaded components
-const Hero = lazy(() => import("./components/Hero"));
-const About = lazy(() => import("./components/About"));
-const Projects = lazy(() => import("./components/Projects"));
-const Contact = lazy(() => import("./components/Contact"));
-const SocialLinks = lazy(() => import("./components/SocialLinks"));
+import React from 'react';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
-      <Suspense fallback={<Loading />}>
-        {" "}
-        {/* Use Loading component here */}
+      <main>
         <Hero />
         <About />
         <Projects />
         <Contact />
-        <SocialLinks />
-      </Suspense>
+      </main>
       <Footer />
     </div>
   );
